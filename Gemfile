6703af49da7d4833a1e121e2bfb959f3dc01d699
fakestarbaby <fakestarbaby@gmail.com>
2012-06-05 18:57:35 +0900

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'heroku'
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAdminium::Application.config.secret_token = '6f1dab6370101c82427cad68113da9cbecaada83a526c1a3af8dd32ab8eecc62558721b0989f6f9b8c44b4f0c6f39b63456bb53b9615378d915a41a7af53b5f0'
